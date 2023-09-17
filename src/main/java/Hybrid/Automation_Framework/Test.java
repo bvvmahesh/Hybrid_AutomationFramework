3d3433b63ac79d7d029eb953ac443b41b88a14b5
Mahesh <bavisetty.veera@gmail.com>
2023-09-17 13:50:11 +0530

    my first commit

---
+++ src/main/java/Hybrid/Automation_Framework/Test.java
@@ -0,0 +1,12 @@
+package Hybrid.Automation_Framework;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is pega Test");
+
+	}
+
+}
