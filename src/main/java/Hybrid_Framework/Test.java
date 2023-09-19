3b6b2f24d090a66b609fc73cc3f795e861ee2fc3
Mahesh <bavisetty.veera@gmail.com>
2023-09-19 13:01:04 +0530

    my third commit

--- src/main/java/Hybrid_Framework/Test.java
+++ src/main/java/Hybrid_Framework/Test.java
@@ -6,8 +6,13 @@ public class Test {
 		// TODO Auto-generated method stub
 		
 		System.out.println("This is pega Test");
+// Updated upstream:src/main/java/Hybrid_Framework/Test.java
 		System.out.println("changes in office laptop");
 		System.out.println("changes in office laptop1");
+
+		System.out.println("personal laptop changes");
+		System.out.println("personal laptop change1");
+// Stashed changes:src/main/java/Hybrid/Automation_Framework/Test.java
 
 	}
 
